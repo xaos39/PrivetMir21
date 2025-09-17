ce049479690c0677354cdbca97af869c0ad80b2e
xaos39 <mif39@bk.ru>
2025-09-18 00:27:10 +0300

    Изменен JS
    Добавлена возможность повторной анимации.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-<script>
-        function showHello() {
-            document.getElementById("message").textContent = "Hello, World!";
-        }
-    </script>
+function showHello() {
+    const message = document.getElementById("message");
+    message.textContent = "Hello, World!";
+    message.classList.add("animation-text");
+
+    // Удаляем класс анимации после завершения для возможности повторной анимации
+    setTimeout(() => {
+        message.classList.remove("animation-text");
+    }, 600);
+}
